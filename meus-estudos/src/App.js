e19d3f5a9f0417064d4db413bd8a8e0504583448
GustavoSanBar <99770511+GustavoSanBar@users.noreply.github.com>
2023-05-16 19:39:12 -0300

    Colocando url

--- meus-estudos/src/App.js
+++ meus-estudos/src/App.js
@@ -2,10 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name='Gustavo'
+  const newname = name.toUpperCase()
+  
+ 
+  function sum(a, b){
+    return a + b
+  }
+
+  const url ='https://i.pinimg.com/564x/d7/75/6b/d7756be2fb35a8cb5f752d100f68a541.jpg'
+
+
   return (
     <div className="App">
       <h1>Olá, React.</h1>
-      <p>Meu primeiro app em React.JS</p>
+      <h2>Modificado por {newname}</h2>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt="Teste"/>
     </div>
   );
 }
